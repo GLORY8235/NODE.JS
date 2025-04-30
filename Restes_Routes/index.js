0aa0ad42f12f066f25360d107b4579a3487762b3
GLORY8235 <gocityfrontenddevelopment@gmail.com>
2025-04-30 16:48:36 +0100

    commiting port 8000

--- Restes_Routes/index.js
+++ Restes_Routes/index.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const app = express();
 
+// Middleware to parse form data
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json)
+
+// GET /tacos route
 app.get('/tacos', (req, res) => {
     res.send('GET /tacos response');
 });
 
-app.listen(5000, () => {
-    console.log("ON PORT 5000");
+// POST /tacos route
+app.post('/tacos', (req, res) => {
+   const {meat, qty } = req.body;
+    res.send(`ok, here are your ${qty} ${meat} tacos`);
+});
+
+// Start the server
+app.listen(8000, () => {
+    console.log("ON PORT 8000");
 });
