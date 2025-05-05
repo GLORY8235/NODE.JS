94c206e389232ae18a95dda904bac0002c25399a
GLORY8235 <gocityfrontenddevelopment@gmail.com>
2025-05-05 14:04:14 +0100

    fresh commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+
+// Middleware to parse form data
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json())
+
+// GET /tacos route
+app.get('/tacos', (req, res) => {
+    res.send('GET /tacos response');
+});
+
+// POST /tacos route
+app.post('/tacoses', (req, res) => {
+   const {meat, qty } = req.body;
+    res.send(`ok, here are your ${qty} ${meat} tacos`);
+});
+
+// Start the server
+app.listen(8000, () => {
+    console.log("ON PORT 8000");
+});
+
+
+// is used in an Express.js application to parse incoming request bodies that are sent via HTML forms (i.e. with
