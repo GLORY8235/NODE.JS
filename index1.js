d2ec7c6fdda278f353bc3fdf38cc37978a3cb6ec
GLORY8235 <gocityfrontenddevelopment@gmail.com>
2025-05-07 12:56:54 +0100

    adding the id to my data

--- index1.js
+++ index1.js
@@ -10,10 +10,17 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 const comments = [
-    { username: 'John Doe', comment: 'This is a great post!' },
-    { username: 'Jane Smith', comment: 'I learned a lot from this.' },
-    { username: 'Bob Johnson', comment: 'Thanks for sharing!' },
-    { username: 'Alice Brown', comment: 'I found this very helpful!' },
+    {
+    id: 1,
+     username: 'John Doe', comment: 'This is a great post!' },
+    { id: 2,
+     username: 'Jane Smith', comment: 'I learned a lot from this.' },
+    {
+    id: 3,
+         username: 'Bob Johnson', comment: 'Thanks for sharing!' },
+    { 
+        id: 4,
+        username: 'Alice Brown', comment: 'I found this very helpful!' },
 ];
 
 app.get('/comments', (req, res) => {
